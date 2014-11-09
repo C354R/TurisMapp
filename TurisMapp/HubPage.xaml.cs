0a826e1dade0cde14d1e36c0ff628b18075168b9
C354R <hsm21c354r@gmail.com>
2014-11-08 21:10:24 -0600

    Inicio

---
+++ TurisMapp/HubPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using TurisMapp.Common;
+using TurisMapp.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla Aplicación Hub está documentada en http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace TurisMapp
+{
+    /// <summary>
+    /// Página en la que muestra una colección de elementos agrupados.
+    /// </summary>
+    public sealed partial class HubPage : Page
+    {
+        private readonly NavigationHelper navigationHelper;
+        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
+
+        public HubPage()
+        {
+            this.InitializeComponent();
+
+            // Hub solo se admite con orientación vertical
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Obtiene el <see cref="NavigationHelper"/> asociado a esta <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Obtiene el modelo de vista para esta <see cref="Page"/>.
+        /// Este puede cambiarse a un modelo de vista fuertemente tipada.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Rellena la página con el contenido pasado durante la navegación.  Cualquier estado guardado se
+        /// proporciona también al crear de nuevo una página a partir de una sesión anterior.
+        /// </summary>
+        /// <param name="sender">
+        /// El origen del evento; suele ser <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Datos de evento que proporcionan tanto el parámetro de navegación pasado a
+        /// <see cref="Frame.Navigate(Type, Object)"/> cuando se solicitó inicialmente esta página y
+        /// un diccionario del estado mantenido por esta página durante una sesión
+        /// anterior. El estado será null la primera vez que se visite una página.</param>
+        private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            // TODO: Crear un modelo de datos adecuado para el dominio del problema para reemplazar los datos de ejemplo
+            var sampleDataGroups = await SampleDataSource.GetGroupsAsync();
+            this.DefaultViewModel["Groups"] = sampleDataGroups;
+        }
+
+        /// <summary>
+        /// Mantiene el estado asociado con esta página en caso de que se suspenda la aplicación o
+        /// se descarte la página de la memoria caché de navegación.  Los valores deben cumplir los requisitos
+        /// de serialización de <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">El origen del evento; suele ser <see cref="NavigationHelper"/></param>
+        /// <param name="e">Datos de evento que proporcionan un diccionario vacío para rellenar con
+        /// un estado serializable.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            // TODO: Guardar aquí el estado único de la página.
+        }
+
+        /// <summary>
+        /// Muestra los detalles de un grupo en el que se ha hecho clic en la <see cref="SectionPage"/>.
+        /// </summary>
+        private void GroupSection_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+            if (!Frame.Navigate(typeof(SectionPage), groupId))
+            {
+                throw new Exception(this.resourceLoader.GetString("NavigationFailedExceptionMessage"));
+            }
+        }
+
+        /// <summary>
+        /// Muestra los detalles de un elemento en el que se ha hecho clic en la <see cref="ItemPage"/>
+        /// </summary>
+        private void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navegar a la página de destino adecuada y configurar la nueva página
+            // al pasar la información requerida como parámetro de navegación
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            if (!Frame.Navigate(typeof(ItemPage), itemId))
+            {
+                throw new Exception(this.resourceLoader.GetString("NavigationFailedExceptionMessage"));
+            }
+        }
+
+        #region Registro de NavigationHelper
+
+        /// <summary>
+        /// Los métodos proporcionados en esta sección se usan simplemente para permitir
+        /// que NavigationHelper responda a los métodos de navegación de la página.
+        /// <para>
+        /// Debe incluirse lógica específica de página en los controladores de eventos para
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// y <see cref="NavigationHelper.SaveState"/>.
+        /// El parámetro de navegación está disponible en el método LoadState
+        /// junto con el estado de página mantenido durante una sesión anterior.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Datos de evento que describen cómo se llegó a esta página.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
